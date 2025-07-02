fcd6138698b32e4880615e45a4d0eaf5e1404624
abhigyan225 <bajpaiabhigyan2002@gmail.com>
2025-07-02 19:57:44 +0530

    Update app.py for Render deployment

--- app/app.py
+++ app/app.py
@@ -507,5 +507,9 @@ def visualize():
 def static_test():
     return '<link rel="stylesheet" href="/static/style.css">'
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
